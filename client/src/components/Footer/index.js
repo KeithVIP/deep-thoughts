85deee52123b98f388d5e90fae617c884ac2a45c
Keith <keithvip@gmail.com>
2022-07-18 18:17:46 -0500

    fixed reboot

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2022 by Keith Yanosy</div>
+    </footer>
+  );
+};
+
+export default Footer;
